b659ee5180b1b0aefea9e1871ef1d1d13b488375
muhammad-furqan-waseem-786 <131297300+muhammad-furqan-waseem-786@users.noreply.github.com>
2023-04-20 13:23:26 +0500

    Add files via upload

---
+++ returnBookForm.Designer.cs
@@ -0,0 +1,142 @@
+﻿
+namespace PROJECT
+{
+    partial class returnBookForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.bookSessionNumber = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(108, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(255, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Book Session Number";
+            // 
+            // bookSessionNumber
+            // 
+            this.bookSessionNumber.Location = new System.Drawing.Point(369, 31);
+            this.bookSessionNumber.Name = "bookSessionNumber";
+            this.bookSessionNumber.Size = new System.Drawing.Size(363, 35);
+            this.bookSessionNumber.TabIndex = 1;
+            this.bookSessionNumber.TextChanged += new System.EventHandler(this.bookSessionNumber_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(754, 31);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(188, 35);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Return Book";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dateTimePicker
+            // 
+            this.dateTimePicker.Location = new System.Drawing.Point(369, 95);
+            this.dateTimePicker.Name = "dateTimePicker";
+            this.dateTimePicker.Size = new System.Drawing.Size(408, 35);
+            this.dateTimePicker.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(108, 101);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(140, 29);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Return Date\r\n";
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.AllowUserToAddRows = false;
+            this.dataGridView.AllowUserToDeleteRows = false;
+            this.dataGridView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Location = new System.Drawing.Point(13, 162);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.ReadOnly = true;
+            this.dataGridView.RowHeadersVisible = false;
+            this.dataGridView.RowHeadersWidth = 62;
+            this.dataGridView.RowTemplate.Height = 28;
+            this.dataGridView.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView.Size = new System.Drawing.Size(1219, 479);
+            this.dataGridView.TabIndex = 5;
+            // 
+            // returnBookForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnableAllowFocusChange;
+            this.BackColor = System.Drawing.Color.DodgerBlue;
+            this.ClientSize = new System.Drawing.Size(1244, 653);
+            this.Controls.Add(this.dataGridView);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dateTimePicker);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.bookSessionNumber);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.Name = "returnBookForm";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Text = "Return Book";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.returnBookForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox bookSessionNumber;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dataGridView;
+    }
+}
---
+++ returnBookForm.cs
@@ -0,0 +1,351 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace PROJECT
+{
+    public partial class returnBookForm : Form
+    {
+        private Validation validationObj = new Validation();
+        private SqlCommand commandObj;
+        private SqlDataReader dataReaderObj;
+        private SqlDataAdapter dataAdapterObj;
+        private DataTable tableObj = new DataTable();
+        private string updateQuery = "\0", selectQuery = "\0", insertQuery = "\0";
+        private string status = "Available", registerationNumber = "\0", readDateFromDataBase = "\0";
+        private string readFineFromDataBase = "\0" , storeDate = "0";
+        private DateTime date = new DateTime();
+        private int storeFine = 0 , calaulateNumberOfDays = 0 , calculatedFine = 0 , totalIssuedBook = 0;
+
+        private void returnBookForm_Load(object sender, EventArgs e)
+        {
+            gridViewDisplay();
+        }
+
+        public returnBookForm()
+        {
+            InitializeComponent();
+        }
+
+        private bool bookIssuedStatus(int bookSessionNmber)
+        {
+            using (SqlConnection connectionObj = new SqlConnection(validationObj.connectionString()))
+            {
+                connectionObj.Open();
+
+                selectQuery = "SELECT Status FROM bookDetails WHERE SessionNumber = '" + bookSessionNmber + "'";
+                commandObj = new SqlCommand(selectQuery, connectionObj);
+                dataReaderObj = commandObj.ExecuteReader();
+
+                while (dataReaderObj.Read())
+                {
+                    status = dataReaderObj.GetValue(0).ToString();
+                }
+                dataReaderObj.Close();
+                connectionObj.Close();
+
+                if (status == "Issued")
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+        private bool registerationNumberExistance(string regisisterationNumber)
+        {
+            using (SqlConnection connectionObj = new SqlConnection(validationObj.connectionString()))
+            {
+                connectionObj.Open();
+
+                selectQuery = "SELECT *FROM FineTable WHERE RegisterationNumber = '" + registerationNumber + "'";
+                dataAdapterObj = new SqlDataAdapter(selectQuery, connectionObj);
+                dataAdapterObj.Fill(tableObj);
+                connectionObj.Close();
+
+                if (tableObj.Rows.Count == 1)
+                {
+                    tableObj.Rows.Clear();
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+        private string issueBookDate(int bookSessionNumber)
+        {
+            using (SqlConnection connectionObj = new SqlConnection(validationObj.connectionString()))
+            {
+                connectionObj.Open();
+
+                selectQuery = "SELECT IssueDate FROM bookDetails WHERE SessionNumber = '" + bookSessionNumber + "'";
+                commandObj = new SqlCommand(selectQuery, connectionObj);
+                dataReaderObj = commandObj.ExecuteReader();
+
+                while (dataReaderObj.Read())
+                {
+                    readDateFromDataBase = dataReaderObj.GetValue(0).ToString();
+                }
+
+                dataReaderObj.Close();
+                connectionObj.Close();
+            }
+
+            return readDateFromDataBase;
+        }
+        private string studentRegisterationNumber(int sessionNumber)
+        {
+            using (SqlConnection connectionObj = new SqlConnection(validationObj.connectionString()))
+            {
+                connectionObj.Open();
+                selectQuery = "SELECT RegisterationNumber FROM bookDetails WHERE SessionNumber = '" + sessionNumber + "'";
+                commandObj = new SqlCommand(selectQuery, connectionObj);
+                dataReaderObj = commandObj.ExecuteReader();
+
+                while (dataReaderObj.Read())
+                {
+                    registerationNumber = dataReaderObj.GetValue(0).ToString();
+                }
+                dataReaderObj.Close();
+                connectionObj.Close();
+            }
+            return registerationNumber;
+        }
+
+        private void bookSessionNumber_TextChanged(object sender, EventArgs e)
+        {
+            if (bookSessionNumber.Text == "" || bookSessionNumber.Text == " ")
+            {
+                return;
+            }
+            else if (!validationObj.integerOnly(bookSessionNumber.Text))
+            {
+                MessageBox.Show("Book session number contains digits only", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                bookSessionNumber.Clear();
+            }
+            else if (!validationObj.checkLength(bookSessionNumber.Text, 5))
+            {
+                MessageBox.Show("Book session number contains 4 digits only", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                bookSessionNumber.Clear();
+            }
+            else if(int.Parse(bookSessionNumber.Text) <= 0)
+            {
+                MessageBox.Show("Book session number should be greater than 0", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                bookSessionNumber.Clear();
+            }
+        }
+
+        private void gridViewDisplay()
+        {
+            using (SqlConnection connectionObj = new SqlConnection(validationObj.connectionString()))
+            {
+                selectQuery = "SELECT Title,SessionNumber,Status,IssueDate,RegisterationNumber FROM bookDetails";
+                SqlDataAdapter dataAdapterObj = new SqlDataAdapter(selectQuery , connectionObj);
+                DataTable dataTableObj = new DataTable();
+                dataAdapterObj.Fill(dataTableObj);
+                dataGridView.DataSource = dataTableObj;
+                dataGridView.Columns[0].HeaderText = "Book Title";
+                dataGridView.Columns[1].HeaderText = "Book Session Number";
+                dataGridView.Columns[2].HeaderText= "Book Status";
+                dataGridView.Columns[3].HeaderText = "Book Issue Date";
+                dataGridView.Columns[4].HeaderText = "Registeration Number";
+                dataGridView.Sort(dataGridView.Columns[1], ListSortDirection.Ascending);
+            }
+        }
+        private void updateBookDataBase(string fRegisterationNumber, string pDate, string status, int bookSessionNumber)
+        {
+            using (SqlConnection connectionObj = new SqlConnection(validationObj.connectionString()))
+            {
+                connectionObj.Open();
+
+                updateQuery = "UPDATE bookDetails SET IssueDate = '" + pDate + "' , Status = '" + status + "' , RegisterationNumber = '" + fRegisterationNumber + "' WHERE SessionNumber = '" + bookSessionNumber + "'";
+                commandObj = new SqlCommand(updateQuery, connectionObj);
+                commandObj.ExecuteNonQuery();
+
+                connectionObj.Close();
+            }
+        }
+        private void fineDataBase(int fine , string registerationNumber , int flag)
+        {
+            using (SqlConnection connectionObj = new SqlConnection(validationObj.connectionString()))
+            {
+                connectionObj.Open();
+
+                if(flag == 1)
+                {
+                    updateQuery = "UPDATE FineTable SET Fine = '" + fine + "' WHERE RegisterationNumber = '" + registerationNumber + "'";
+                    commandObj = new SqlCommand(updateQuery, connectionObj);                    
+                }
+                else
+                {
+                    insertQuery = "INSERT INTO FineTable VALUES(@RegisterationNumber , @Fine)";
+                    commandObj = new SqlCommand(insertQuery, connectionObj);
+                    commandObj.Parameters.AddWithValue("@RegisterationNumber" , registerationNumber);
+                    commandObj.Parameters.AddWithValue("@Fine" , fine);
+                }
+
+                commandObj.ExecuteNonQuery();
+                connectionObj.Close();
+            }
+        }        
+        private void decrementBook(int issuedBookQuantity, string fRegisterationNumber)
+        {
+            using (SqlConnection connectionObj = new SqlConnection(validationObj.connectionString()))
+            {
+                connectionObj.Open();
+
+                issuedBookQuantity = issuedBookQuantity - 1;
+                updateQuery = "UPDATE Student SET IssuedBook = '" + issuedBookQuantity + "' WHERE RegisterationNumber = '" + fRegisterationNumber + "'";
+                commandObj = new SqlCommand(updateQuery, connectionObj);
+                commandObj.ExecuteNonQuery();
+
+                connectionObj.Close();
+            }
+        }
+        private int existingFine(string registerationNumber)
+        {
+            using (SqlConnection connectionObj = new SqlConnection(validationObj.connectionString()))
+            {
+                connectionObj.Open();
+
+                selectQuery = "SELECT Fine FROM FineTable WHERE RegisterationNumber = '" + registerationNumber + "'";
+                commandObj = new SqlCommand(selectQuery, connectionObj);
+                dataReaderObj = commandObj.ExecuteReader();
+
+                while (dataReaderObj.Read())
+                {
+                    readFineFromDataBase = dataReaderObj.GetValue(0).ToString();
+                }
+
+                dataReaderObj.Close();
+                connectionObj.Close();
+
+                storeFine = int.Parse(readFineFromDataBase);
+
+                if (storeFine > 0)
+                {
+                    return storeFine;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+        }
+        private int totalBookIssuedByStudent(string fRegisterationNumber)
+        {
+            using (SqlConnection connectionObj = new SqlConnection(validationObj.connectionString()))
+            {
+                connectionObj.Open();
+
+                selectQuery = "SELECT issuedBook FROM Student WHERE RegisterationNumber = '" + fRegisterationNumber + "'";
+                commandObj = new SqlCommand(selectQuery, connectionObj);
+                dataReaderObj = commandObj.ExecuteReader();
+
+                while (dataReaderObj.Read())
+                {
+                    totalIssuedBook = int.Parse(dataReaderObj.GetValue(0).ToString());
+                }
+                dataReaderObj.Close();
+
+                connectionObj.Close();
+            }
+            return totalIssuedBook;
+        }
+        private bool sessionNumberExistance(int fSessionNumber)
+        {
+            using (SqlConnection connectionObj = new SqlConnection(validationObj.connectionString()))
+            {
+                connectionObj.Open();
+
+                selectQuery = "SELECT *From bookDetails WHERE sessionNumber = '" + fSessionNumber + "' ";
+                dataAdapterObj = new SqlDataAdapter(selectQuery, connectionObj);
+                dataAdapterObj.Fill(tableObj);
+                connectionObj.Close();
+
+                if (tableObj.Rows.Count > 0)
+                {
+                    tableObj.Rows.Clear();
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int sessionNumber = int.Parse(bookSessionNumber.Text);
+
+                if (sessionNumberExistance(sessionNumber))
+                {
+                    if (bookIssuedStatus(sessionNumber))
+                    {
+                        registerationNumber = studentRegisterationNumber(sessionNumber);
+                        storeDate = issueBookDate(sessionNumber);
+                        date = DateTime.Parse(storeDate);
+                        calaulateNumberOfDays = (int)(dateTimePicker.Value - date).TotalDays;
+
+                        if (calaulateNumberOfDays < 0)
+                        {
+                            MessageBox.Show("Select the correct date ", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                        else
+                        {
+                            if (calaulateNumberOfDays > 14)
+                            {
+                                calaulateNumberOfDays = calaulateNumberOfDays - 14;
+                                calculatedFine = calaulateNumberOfDays * 10;
+
+                                if (registerationNumberExistance(registerationNumber))
+                                {
+                                    storeFine = existingFine(registerationNumber);
+                                    calculatedFine = calculatedFine + storeFine;
+                                    fineDataBase(calculatedFine, registerationNumber, 1);
+                                }
+                                else
+                                {
+                                    fineDataBase(calculatedFine, registerationNumber, 0);
+                                }
+
+                                MessageBox.Show(registerationNumber + " has to pay " + calculatedFine + " rupees fine", "Pay Fine", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            }
+
+                            totalIssuedBook = totalBookIssuedByStudent(registerationNumber);
+                            decrementBook(totalIssuedBook, registerationNumber);
+                            updateBookDataBase("None", "0/0/0", "Available", sessionNumber);
+                            MessageBox.Show("Book returned", "Return Book", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            gridViewDisplay();
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("This book never issued" , "Error" , MessageBoxButtons.OK , MessageBoxIcon.Error);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("This book doesn't exist", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            catch (Exception obj)
+            {
+                MessageBox.Show(obj.Message);
+            }
+        }
+    }
+}
