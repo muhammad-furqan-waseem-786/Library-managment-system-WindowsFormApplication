a0e1195685d8fa93745593e70dd042a990a832a9
muhammad-furqan-waseem-786 <131297300+muhammad-furqan-waseem-786@users.noreply.github.com>
2023-04-20 13:14:35 +0500

    Add files via upload

---
+++ createAccountForm.Designer.cs
@@ -0,0 +1,151 @@
+﻿
+namespace PROJECT
+{
+    partial class createAccountForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(createAccountForm));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.passwordTextBox = new System.Windows.Forms.TextBox();
+            this.confirmPasswordTextBox = new System.Windows.Forms.TextBox();
+            this.userNameTextBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(62, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(124, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Username";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(62, 154);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(208, 29);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Confirm password";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(62, 100);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(120, 29);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Password";
+            // 
+            // passwordTextBox
+            // 
+            this.passwordTextBox.Location = new System.Drawing.Point(224, 91);
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.Size = new System.Drawing.Size(318, 35);
+            this.passwordTextBox.TabIndex = 4;
+            // 
+            // confirmPasswordTextBox
+            // 
+            this.confirmPasswordTextBox.Location = new System.Drawing.Point(224, 148);
+            this.confirmPasswordTextBox.Name = "confirmPasswordTextBox";
+            this.confirmPasswordTextBox.Size = new System.Drawing.Size(318, 35);
+            this.confirmPasswordTextBox.TabIndex = 5;
+            // 
+            // userNameTextBox
+            // 
+            this.userNameTextBox.Location = new System.Drawing.Point(224, 33);
+            this.userNameTextBox.Name = "userNameTextBox";
+            this.userNameTextBox.Size = new System.Drawing.Size(318, 35);
+            this.userNameTextBox.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(132, 253);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(195, 51);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Create account";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(402, 253);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(195, 51);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Login Page";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // createAccountForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DodgerBlue;
+            this.ClientSize = new System.Drawing.Size(714, 494);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.confirmPasswordTextBox);
+            this.Controls.Add(this.passwordTextBox);
+            this.Controls.Add(this.userNameTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.Name = "createAccountForm";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Create Account";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox passwordTextBox;
+        private System.Windows.Forms.TextBox confirmPasswordTextBox;
+        private System.Windows.Forms.TextBox userNameTextBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
---
+++ createAccountForm.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace PROJECT
+{
+    
+    public partial class createAccountForm : Form
+    {
+        private SqlCommand commandObj;
+        private string insertQuery = "\0", selectQuery = "\0";
+        private SqlDataAdapter dataAdapterObj;
+        private DataTable dataTableObj = new DataTable();
+        private string userName = "\0", password = "\0", confirmPassword = "\0";
+        private LoginForm loginObj = new LoginForm();
+        private Validation validationObj = new Validation();
+
+        public createAccountForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            loginObj.Show();
+        }
+
+        private void createAccount(string fuserName , string fPassword)
+        {
+            using (SqlConnection connectionObj = new SqlConnection(validationObj.connectionString()))
+            {
+                connectionObj.Open();
+                insertQuery = "INSERT INTO LoginDataBase VALUES(@userName , @password)";
+
+                commandObj = new SqlCommand(insertQuery, connectionObj);
+
+                commandObj.Parameters.AddWithValue("@userName", fuserName);
+                commandObj.Parameters.AddWithValue("@password" , fPassword);
+                commandObj.ExecuteNonQuery();
+                connectionObj.Close();
+            }
+        }
+        private bool accountExistance(string fUserName , string fPassword)
+        {
+            using (SqlConnection connectionObj = new SqlConnection(validationObj.connectionString()))
+            { //SQL connection is open
+                connectionObj.Open();
+
+                //In this database SELECT query is written
+                selectQuery = "SELECT *FROM LoginDataBase WHERE userName = '" + fUserName + "' and password = '" + fPassword + "'";
+
+                /*The DataAdapter serves as a bridge between a DataSet 
+                 * and a data source for retrieving and saving data*/
+                dataAdapterObj = new SqlDataAdapter(selectQuery, connectionObj);
+
+                /*The DataAdapter provides this bridge by mapping Fill, 
+                 * which changes the data in the DataSet to match the data 
+                 * in the data source, and Update, which changes the 
+                 * data in the data source to match the data in the DataSet*/
+
+                dataAdapterObj.Fill(dataTableObj);
+
+                //SQL connection is closed
+                connectionObj.Close();
+
+                //If the required row os found than count value will incremented
+                if (dataTableObj.Rows.Count == 1)
+                {
+                    dataTableObj.Rows.Clear();
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+                
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                userName = validationObj.encrypt(userNameTextBox.Text);
+                password = validationObj.encrypt(passwordTextBox.Text);
+                confirmPassword = validationObj.encrypt(confirmPasswordTextBox.Text);
+
+                if(validationObj.nonEmptyOrNullString(userNameTextBox.Text) || validationObj.nonEmptyOrNullString(passwordTextBox.Text) || validationObj.nonEmptyOrNullString(confirmPasswordTextBox.Text))
+                {
+                    MessageBox.Show("None of the field can be empty ", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+                else if (password == confirmPassword)
+                {
+                    if(accountExistance(userName , password))
+                    {
+                        MessageBox.Show("Account and password already exist", "Data exist", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    }
+                    else
+                    {
+                        createAccount(userName,password);
+                        MessageBox.Show("Account created", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Password and confirm password should be same", "Invalid password", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+            catch(Exception obj)
+            {
+                MessageBox.Show(obj.Message);
+            }
+        }
+    }
+}
