7e76608969164ab7652d63cfa7ab5d755c6ae84a
muhammad-furqan-waseem-786 <131297300+muhammad-furqan-waseem-786@users.noreply.github.com>
2023-04-20 13:13:39 +0500

    Add files via upload

---
+++ addBookForm.Designer.cs
@@ -0,0 +1,257 @@
+﻿
+namespace PROJECT
+{
+    partial class addBookForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.titleTextBox = new System.Windows.Forms.TextBox();
+            this.authorTextBox = new System.Windows.Forms.TextBox();
+            this.editionTextBox = new System.Windows.Forms.TextBox();
+            this.copiesTextBox = new System.Windows.Forms.TextBox();
+            this.sessionNumberTextBox = new System.Windows.Forms.TextBox();
+            this.priceTextBox = new System.Windows.Forms.TextBox();
+            this.addBookButton = new System.Windows.Forms.Button();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(31, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(123, 29);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Book Title";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(31, 141);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(144, 29);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Book Author";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(31, 198);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(150, 29);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Book Edition";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(31, 254);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(204, 29);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Number of copies";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(31, 307);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(193, 29);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Session Number";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.ForeColor = System.Drawing.Color.White;
+            this.label7.Location = new System.Drawing.Point(31, 358);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(69, 29);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Price";
+            // 
+            // titleTextBox
+            // 
+            this.titleTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleTextBox.Location = new System.Drawing.Point(181, 78);
+            this.titleTextBox.Name = "titleTextBox";
+            this.titleTextBox.Size = new System.Drawing.Size(277, 39);
+            this.titleTextBox.TabIndex = 8;
+            this.titleTextBox.TextChanged += new System.EventHandler(this.titleTextBox_TextChanged);
+            // 
+            // authorTextBox
+            // 
+            this.authorTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.authorTextBox.Location = new System.Drawing.Point(181, 135);
+            this.authorTextBox.Name = "authorTextBox";
+            this.authorTextBox.Size = new System.Drawing.Size(277, 39);
+            this.authorTextBox.TabIndex = 9;
+            this.authorTextBox.TextChanged += new System.EventHandler(this.authorTextBox_TextChanged);
+            // 
+            // editionTextBox
+            // 
+            this.editionTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.editionTextBox.Location = new System.Drawing.Point(181, 191);
+            this.editionTextBox.Name = "editionTextBox";
+            this.editionTextBox.Size = new System.Drawing.Size(277, 39);
+            this.editionTextBox.TabIndex = 10;
+            this.editionTextBox.TextChanged += new System.EventHandler(this.editionTextBox_TextChanged);
+            // 
+            // copiesTextBox
+            // 
+            this.copiesTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.copiesTextBox.Location = new System.Drawing.Point(181, 247);
+            this.copiesTextBox.Name = "copiesTextBox";
+            this.copiesTextBox.Size = new System.Drawing.Size(277, 39);
+            this.copiesTextBox.TabIndex = 11;
+            this.copiesTextBox.TextChanged += new System.EventHandler(this.copiesTextBox_TextChanged);
+            // 
+            // sessionNumberTextBox
+            // 
+            this.sessionNumberTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sessionNumberTextBox.Location = new System.Drawing.Point(181, 301);
+            this.sessionNumberTextBox.Name = "sessionNumberTextBox";
+            this.sessionNumberTextBox.Size = new System.Drawing.Size(277, 39);
+            this.sessionNumberTextBox.TabIndex = 12;
+            this.sessionNumberTextBox.TextChanged += new System.EventHandler(this.sessionNumberTextBox_TextChanged);
+            // 
+            // priceTextBox
+            // 
+            this.priceTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.priceTextBox.Location = new System.Drawing.Point(181, 352);
+            this.priceTextBox.Name = "priceTextBox";
+            this.priceTextBox.Size = new System.Drawing.Size(277, 39);
+            this.priceTextBox.TabIndex = 13;
+            this.priceTextBox.TextChanged += new System.EventHandler(this.priceTextBox_TextChanged);
+            // 
+            // addBookButton
+            // 
+            this.addBookButton.ForeColor = System.Drawing.Color.Black;
+            this.addBookButton.Location = new System.Drawing.Point(35, 469);
+            this.addBookButton.Name = "addBookButton";
+            this.addBookButton.Size = new System.Drawing.Size(158, 43);
+            this.addBookButton.TabIndex = 14;
+            this.addBookButton.Text = "Add Book";
+            this.addBookButton.UseVisualStyleBackColor = true;
+            this.addBookButton.Click += new System.EventHandler(this.addBookButton_Click);
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.AllowUserToAddRows = false;
+            this.dataGridView.AllowUserToDeleteRows = false;
+            this.dataGridView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Location = new System.Drawing.Point(463, 23);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.ReadOnly = true;
+            this.dataGridView.RowHeadersVisible = false;
+            this.dataGridView.RowHeadersWidth = 62;
+            this.dataGridView.RowTemplate.Height = 28;
+            this.dataGridView.Size = new System.Drawing.Size(655, 604);
+            this.dataGridView.TabIndex = 15;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(239, 469);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(157, 43);
+            this.button1.TabIndex = 16;
+            this.button1.Text = "Clear Text Box";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // addBookForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DodgerBlue;
+            this.ClientSize = new System.Drawing.Size(1128, 645);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView);
+            this.Controls.Add(this.addBookButton);
+            this.Controls.Add(this.priceTextBox);
+            this.Controls.Add(this.sessionNumberTextBox);
+            this.Controls.Add(this.copiesTextBox);
+            this.Controls.Add(this.editionTextBox);
+            this.Controls.Add(this.authorTextBox);
+            this.Controls.Add(this.titleTextBox);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.Black;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.Name = "addBookForm";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Text = "Add Book";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.addBookForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox titleTextBox;
+        private System.Windows.Forms.TextBox authorTextBox;
+        private System.Windows.Forms.TextBox editionTextBox;
+        private System.Windows.Forms.TextBox copiesTextBox;
+        private System.Windows.Forms.TextBox sessionNumberTextBox;
+        private System.Windows.Forms.TextBox priceTextBox;
+        private System.Windows.Forms.Button addBookButton;
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ addBookForm.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace PROJECT
+{
+    public partial class addBookForm : Form
+    {
+        private Validation validationObj = new Validation();
+        private SqlCommand commandObj;
+        private SqlDataAdapter dataAdapter;
+        private DataTable tableObj = new DataTable();
+        private string insertQuery = "\0", selectQuery = "\0";
+        private string title = "\0", author = "\0", edition = "\0";
+        private float price = 0;
+        private int copies = 0;
+        private int session = 0;
+
+        public addBookForm()
+        {
+            InitializeComponent();
+        }
+        private void insertData(string fTitle, string fAuthor, string fEdition, int fCopies, int fSessionNumber, float fPrice, string fStatus, string fIssueDate, string fRegisterationNumber)
+        {
+            using (SqlConnection connectionObj = new SqlConnection(validationObj.connectionString()))
+            {
+                connectionObj.Open();
+
+                for (int i = 1; i <= int.Parse(copiesTextBox.Text); i++)
+                {
+
+                    insertQuery = "INSERT INTO bookDetails VALUES(@Title,@Author,@Edition,@Copies,@SessionNumber,@Price,@Status,@IssueDate,@RegisterationNumber)";
+                    commandObj = new SqlCommand(insertQuery, connectionObj);
+
+                    commandObj.Parameters.AddWithValue("@Title", fTitle);
+                    commandObj.Parameters.AddWithValue("@Author", fAuthor);
+                    commandObj.Parameters.AddWithValue("@Edition", fEdition);
+                    commandObj.Parameters.AddWithValue("@Copies", fCopies);
+                    commandObj.Parameters.AddWithValue("@SessionNumber", fSessionNumber);
+                    commandObj.Parameters.AddWithValue("@Price", fPrice);
+                    commandObj.Parameters.AddWithValue("@Status", fStatus);
+                    commandObj.Parameters.AddWithValue("@IssueDate", fIssueDate);
+                    commandObj.Parameters.AddWithValue("@RegisterationNumber", fRegisterationNumber);
+                    fSessionNumber++;
+
+                    commandObj.ExecuteNonQuery();
+                }
+
+                connectionObj.Close();
+            }
+        }
+        private bool sessionNumberExistance(int fSessionNumber , int copies)
+        {
+            using (SqlConnection connectionObj = new SqlConnection(validationObj.connectionString()))
+            {
+                connectionObj.Open();
+
+                for (int i = 1; i <= copies; i++)
+                {
+                    selectQuery = "SELECT *From bookDetails WHERE sessionNumber = '" + fSessionNumber + "' ";
+                    dataAdapter = new SqlDataAdapter(selectQuery, connectionObj);
+                    dataAdapter.Fill(tableObj);
+                    fSessionNumber++;
+                }
+                
+                connectionObj.Close();
+
+                if (tableObj.Rows.Count == 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    tableObj.Rows.Clear();
+                    return false;
+                }
+            }
+        }
+        private void loadGridView()
+        {
+            using (SqlConnection connectionObj = new SqlConnection(validationObj.connectionString()))
+            {
+                connectionObj.Open();
+
+                SqlDataAdapter dataAdapterObj = new SqlDataAdapter("SELECT Title,Author,Edition,Copies,SessionNumber,Price FROM bookDetails", connectionObj);
+                DataTable dataTableObj = new DataTable();
+                dataAdapterObj.Fill(dataTableObj);
+                dataGridView.DataSource = dataTableObj;
+
+                dataGridView.Columns[0].HeaderText = "Book Title";
+                dataGridView.Columns[1].HeaderText = "Book Author";
+                dataGridView.Columns[2].HeaderText = "Book Edition";
+                dataGridView.Columns[3].HeaderText = "Number of copies";
+                dataGridView.Columns[4].HeaderText = "Book Session Number";
+                dataGridView.Columns[5].HeaderText = "Price";
+                dataGridView.Sort(dataGridView.Columns[4], ListSortDirection.Ascending);
+                connectionObj.Close();
+            }
+        }
+
+        private void addBookButton_Click(object sender, EventArgs e)
+        {
+            if (validationObj.nonEmptyOrNullString(title) || validationObj.nonEmptyOrNullString(author) || validationObj.nonEmptyOrNullString(edition) || validationObj.nonEmptyOrNullString(sessionNumberTextBox.Text) || validationObj.nonEmptyOrNullString(priceTextBox.Text) || validationObj.nonEmptyOrNullString(copiesTextBox.Text))
+            {
+                MessageBox.Show("None of the field can be empty", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                title = titleTextBox.Text;
+                author = authorTextBox.Text;
+                edition = editionTextBox.Text;
+                copies = int.Parse(copiesTextBox.Text);
+                session = int.Parse(sessionNumberTextBox.Text);
+                price = float.Parse(priceTextBox.Text);
+
+                if (sessionNumberExistance(session, copies))
+                {
+                    insertData(title, author, edition, copies, session, price, "Available", "0/0/0", "None");
+                    MessageBox.Show("Data saved", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    loadGridView();
+                }
+                else
+                {
+                    MessageBox.Show("Session Number already exist " , "Error" , MessageBoxButtons.OK , MessageBoxIcon.Error);
+                    sessionNumberTextBox.Clear();
+                }
+            }
+        }
+
+        private void titleTextBox_TextChanged(object sender, EventArgs e)
+        {
+            if (!validationObj.stringOnly(titleTextBox.Text))
+            {
+                MessageBox.Show("Title contains characters only", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                titleTextBox.Clear();
+            }
+        }
+
+        private void authorTextBox_TextChanged(object sender, EventArgs e)
+        {
+            if (!validationObj.stringOnly(authorTextBox.Text))
+            {
+                MessageBox.Show("Author contains characters only", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                authorTextBox.Clear();
+            }
+        }
+
+        private void editionTextBox_TextChanged(object sender, EventArgs e)
+        {
+            if (editionTextBox.Text == "" || editionTextBox.Text == " ")
+            {
+                return;
+            }
+            else if (!validationObj.integerOnly(editionTextBox.Text))
+            {
+                MessageBox.Show("Edition contains digits only", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                editionTextBox.Clear();
+            }
+            else if(int.Parse(editionTextBox.Text) < 0)
+            {
+                MessageBox.Show("Edition should be greater than zero", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                editionTextBox.Clear();
+            }
+        }
+
+        private void copiesTextBox_TextChanged(object sender, EventArgs e)
+        {
+            if (copiesTextBox.Text == "" || copiesTextBox.Text == " ")
+            {
+                return;
+            }
+            else if (!validationObj.integerOnly(copiesTextBox.Text))
+            {
+                MessageBox.Show("Copies contains digits only", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                copiesTextBox.Clear();
+            }
+            else if (int.Parse(copiesTextBox.Text) <= 0)
+            {
+                MessageBox.Show("Number of copies should be greater than 0", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                copiesTextBox.Clear();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            titleTextBox.Clear();
+            authorTextBox.Clear();
+            sessionNumberTextBox.Clear();
+            editionTextBox.Clear();
+            copiesTextBox.Clear();
+            priceTextBox.Clear();
+        }
+
+        private void sessionNumberTextBox_TextChanged(object sender, EventArgs e)
+        {
+            if (sessionNumberTextBox.Text == "" || sessionNumberTextBox.Text == " ")
+            {
+                return;
+            }
+            else if (!validationObj.integerOnly(sessionNumberTextBox.Text))
+            {
+                MessageBox.Show("Session Number contains digits only", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                sessionNumberTextBox.Clear();
+            }
+            else if (!validationObj.checkLength(sessionNumberTextBox.Text, 5))
+            {
+                MessageBox.Show("Session Number contains 4 digits", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                sessionNumberTextBox.Clear();
+            }
+            else if (int.Parse(sessionNumberTextBox.Text) <= 0)
+            {
+                MessageBox.Show("Number of copies should be greater than 0", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                sessionNumberTextBox.Clear();
+            }
+        }
+
+        private void priceTextBox_TextChanged(object sender, EventArgs e)
+        {
+            if (priceTextBox.Text == "" || priceTextBox.Text == " ")
+            {
+                return;
+            }
+            else if (!validationObj.floatOnly(priceTextBox.Text))
+            {
+                MessageBox.Show("Price contains digits only", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                priceTextBox.Clear();
+            }
+            else if (int.Parse(priceTextBox.Text) <= 0)
+            {
+                MessageBox.Show("Number of copies should be greater than 0", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                priceTextBox.Clear();
+            }
+        }
+
+        private void addBookForm_Load(object sender, EventArgs e)
+        {
+            loadGridView();
+        }
+    }
+}
+
