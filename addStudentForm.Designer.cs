b8ac9764d17a2e254a8c304006ef5612d4c25e20
muhammad-furqan-waseem-786 <131297300+muhammad-furqan-waseem-786@users.noreply.github.com>
2023-04-20 13:14:06 +0500

    Add files via upload

---
+++ addStudentForm.Designer.cs
@@ -0,0 +1,212 @@
+﻿
+namespace PROJECT
+{
+    partial class addStudentForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.idTextBox = new System.Windows.Forms.TextBox();
+            this.departmentTextBox = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.sessionTextBox = new System.Windows.Forms.ComboBox();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(57, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(166, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Student Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(498, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(121, 29);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Student Id";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(57, 88);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 29);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Session";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(498, 88);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(138, 29);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Department";
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(179, 31);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(281, 35);
+            this.nameTextBox.TabIndex = 4;
+            this.nameTextBox.TextChanged += new System.EventHandler(this.nameTextBox_TextChanged);
+            // 
+            // idTextBox
+            // 
+            this.idTextBox.Location = new System.Drawing.Point(620, 31);
+            this.idTextBox.Name = "idTextBox";
+            this.idTextBox.Size = new System.Drawing.Size(281, 35);
+            this.idTextBox.TabIndex = 5;
+            this.idTextBox.TextChanged += new System.EventHandler(this.idTextBox_TextChanged);
+            // 
+            // departmentTextBox
+            // 
+            this.departmentTextBox.FormattingEnabled = true;
+            this.departmentTextBox.Items.AddRange(new object[] {
+            "CS",
+            "EE",
+            "CE"});
+            this.departmentTextBox.Location = new System.Drawing.Point(620, 88);
+            this.departmentTextBox.Name = "departmentTextBox";
+            this.departmentTextBox.Size = new System.Drawing.Size(281, 37);
+            this.departmentTextBox.TabIndex = 7;
+            this.departmentTextBox.Text = "CS";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(992, 20);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(163, 46);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Add Student";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(992, 85);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(163, 45);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Clear Text Boxes";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // sessionTextBox
+            // 
+            this.sessionTextBox.FormattingEnabled = true;
+            this.sessionTextBox.Items.AddRange(new object[] {
+            "2019",
+            "2020"});
+            this.sessionTextBox.Location = new System.Drawing.Point(179, 85);
+            this.sessionTextBox.Name = "sessionTextBox";
+            this.sessionTextBox.Size = new System.Drawing.Size(281, 37);
+            this.sessionTextBox.TabIndex = 11;
+            this.sessionTextBox.Text = "2019";
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.AllowUserToAddRows = false;
+            this.dataGridView.AllowUserToDeleteRows = false;
+            this.dataGridView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Location = new System.Drawing.Point(12, 152);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.ReadOnly = true;
+            this.dataGridView.RowHeadersVisible = false;
+            this.dataGridView.RowHeadersWidth = 62;
+            this.dataGridView.RowTemplate.Height = 28;
+            this.dataGridView.Size = new System.Drawing.Size(1248, 497);
+            this.dataGridView.TabIndex = 12;
+            // 
+            // addStudentForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DodgerBlue;
+            this.ClientSize = new System.Drawing.Size(1286, 661);
+            this.Controls.Add(this.dataGridView);
+            this.Controls.Add(this.sessionTextBox);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.departmentTextBox);
+            this.Controls.Add(this.idTextBox);
+            this.Controls.Add(this.nameTextBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.Name = "addStudentForm";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Text = "Add Student";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.addStudentForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.TextBox idTextBox;
+        private System.Windows.Forms.ComboBox departmentTextBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox sessionTextBox;
+        private System.Windows.Forms.DataGridView dataGridView;
+    }
+}
---
+++ addStudentForm.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace PROJECT
+{
+    public partial class addStudentForm : Form
+    {
+        //Private variables and objects are declared
+
+        private Validation validationObj = new Validation();
+        private string insertQuery = "\0" , selectQuery = "\0";
+        private string registerationNumber = "\0", name = "\0", department = "\0";
+        private int session = 0;
+        private SqlCommand commandObj;
+
+        public addStudentForm()
+        {
+            InitializeComponent();
+        }
+        
+        private void displayGridView()
+        {
+            using (SqlConnection connectionObj = new SqlConnection(validationObj.connectionString()))
+            {
+                connectionObj.Open();
+
+                selectQuery = "SELECT RegisterationNumber , Name , Session , Department FROM Student";
+
+                using(SqlDataAdapter dataAdapterObj = new SqlDataAdapter(selectQuery , connectionObj))
+                {
+                    using(DataTable tableObj = new DataTable())
+                    {
+                        dataAdapterObj.Fill(tableObj);
+                        dataGridView.DataSource = tableObj;
+                    }
+                }
+                connectionObj.Close();
+            }
+        }
+        private void insertStudent(string fRegisterationNumber , string fName , int fSession , string fDepartment , int fIssuedBook)
+        {
+            /* C# using statement makes sure that the allocated objects 
+            * within the using statement is disposed once the using block 
+            * code is executed.*/
+
+            using (SqlConnection connectionObj = new SqlConnection(validationObj.connectionString()))
+            {
+                //SQL connection is open
+                connectionObj.Open();
+
+                //In this database INSERT query is written
+                insertQuery = "INSERT INTO Student VALUES(@RegisterationNumber,@Name,@Session,@Department,@IssuedBook)";
+
+                commandObj = new SqlCommand(insertQuery, connectionObj);
+
+                //Insertion into the tables are done here
+                commandObj.Parameters.AddWithValue("@Name", fName);
+                commandObj.Parameters.AddWithValue("@RegisterationNumber", fRegisterationNumber);
+                commandObj.Parameters.AddWithValue("@Session", fSession);
+                commandObj.Parameters.AddWithValue("@Department", fDepartment);
+                commandObj.Parameters.AddWithValue("@IssuedBook", fIssuedBook);
+                commandObj.ExecuteNonQuery();
+
+                MessageBox.Show("Data Saved", "Add Student", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                connectionObj.Close();
+            }
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // IF any of the text box is empty than this condition will be executed
+                if (validationObj.nonEmptyOrNullString(nameTextBox.Text) || validationObj.nonEmptyOrNullString(idTextBox.Text) || validationObj.nonEmptyOrNullString(sessionTextBox.Text) || validationObj.nonEmptyOrNullString(departmentTextBox.Text))
+                {
+                    MessageBox.Show("None of the field can be empty", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else if(sessionTextBox.Text != "2019" && sessionTextBox.Text != "2020")
+                {
+                    MessageBox.Show("Select the correct session", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);                   
+                }
+                else if(departmentTextBox.Text != "CS" && departmentTextBox.Text != "CE" && departmentTextBox.Text != "EE")
+                {
+                    MessageBox.Show("Select the correct department", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else
+                {
+                    name = nameTextBox.Text;
+                    registerationNumber = int.Parse(sessionTextBox.Text) + "-UET-ShCET-LHR-" + departmentTextBox.Text + "-" + int.Parse(idTextBox.Text);
+                    session = int.Parse(sessionTextBox.Text);
+                    department = departmentTextBox.Text;
+
+                    insertStudent(registerationNumber, name, session, department, 0);
+                    displayGridView();
+                }
+            }
+            catch(Exception obj)
+            {
+                MessageBox.Show(obj.Message);
+            }
+        }
+
+        private void nameTextBox_TextChanged(object sender, EventArgs e)
+        {
+            //If name text box is not string then it clears the text box and display the
+            //invalid message on the screen
+            
+            if (!validationObj.stringOnly(nameTextBox.Text))
+            {
+                MessageBox.Show("Name contains characters only", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                nameTextBox.Clear();
+            }
+        }
+
+        private void sessionTextBox_TextChanged(object sender, EventArgs e)
+        {
+            //If session text box is not integer then it clears the text box and display the
+            //invalid message on the screen
+            
+            if (!validationObj.integerOnly(sessionTextBox.Text))
+            {
+                MessageBox.Show("Session contains digits only", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //This button clears the all text box on the screen
+            nameTextBox.Clear();
+            idTextBox.Clear();
+        }
+
+        private void addStudentForm_Load(object sender, EventArgs e)
+        {
+            displayGridView();
+        }
+
+        private void idTextBox_TextChanged(object sender, EventArgs e)
+        {
+            //If id text box is not integer then it clears the text box and display the
+            //invalid message on the screen
+            
+            if (idTextBox.Text == "" || idTextBox.Text == " ")
+            {
+                return;
+            }
+            else if (!validationObj.checkLength(idTextBox.Text, 3))
+            {
+                MessageBox.Show("Student id consists of two digits only", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                idTextBox.Clear();
+            }
+            else if (!validationObj.integerOnly(idTextBox.Text))
+            {
+                MessageBox.Show("Id contains digits only", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                idTextBox.Clear();
+            }
+            else if (int.Parse(idTextBox.Text) <= 0)
+            {
+                MessageBox.Show("Student id should be greater than zero", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                idTextBox.Clear();
+            }
+        }
+    }
+}
